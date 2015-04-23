e239e946d6b343263a37d210a7cf6459d34a84b7
Reza Keshmir <rkeshmir@yahoo.com>
2015-04-23 10:47:47 +0430

    test(demo): add MainCtrl to Demo App

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -2,7 +2,9 @@
 angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'plunker', 'ngTouch'], function($httpProvider){
   FastClick.attach(document.body);
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}).run(['$location', function($location){
+})
+    .controller("MainCtrl",MainCtrl)
+    .run(['$location', function($location){
   //Allows us to navigate to the correct element on initialization
   if ($location.path() !== '' && $location.path() !== '/') {
     smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
